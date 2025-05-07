ada2095533df6a21d1e215b400adbf0355705ae0
Aashish N <165367795+aashish-neu@users.noreply.github.com>
2025-05-07 11:10:17 +0545

    Add about us resolve in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   plugins: [tailwindcss()],
   base: "",
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "about-us.html"),
+      },
+    },
+  },
 });
