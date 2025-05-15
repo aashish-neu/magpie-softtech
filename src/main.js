5740379dd3a28590fad4d93412390c22f240ae6f
Aashish N <165367795+aashish-neu@users.noreply.github.com>
2025-05-15 10:41:29 +0545

    Update home page svg sizes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import './style.css'
+import "./style.css";
 
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+const mobileHamburger = document.getElementById("mobile-hamburger");
 
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
 
-
+mobileHamburger.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+});
