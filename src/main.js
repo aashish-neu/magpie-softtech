8b93245d6fc1059fbd5b80f18791d994f3586399
Aashish N <165367795+aashish-neu@users.noreply.github.com>
2025-05-15 16:18:06 +0545

    Update mobile typography for about page

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import "./style.css";
 const hamburger = document.getElementById("hamburger");
 const menu = document.getElementById("menu");
 const mobileHamburger = document.getElementById("mobile-hamburger");
+const body = document.getElementById("main-body");
 
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("hidden");
+  body.classList.toggle("overflow-hidden");
 });
 
 mobileHamburger.addEventListener("click", () => {
     menu.classList.toggle("hidden");
+    body.classList.toggle("overflow-hidden");
 });
+
